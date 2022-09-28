5d26a33aec4656cb5494f8fee7f43b3565002936
prof-ajaved <112894546+prof-ajaved@users.noreply.github.com>
2022-09-28 10:59:03 -0400

    Update deliver.ps1

--- jenkins/scripts/deliver.ps1
+++ jenkins/scripts/deliver.ps1
@@ -18,4 +18,4 @@ $env:VERSION=.\mvnw.cmd help:evaluate -Dexpression="project.version" -q -DforceS
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 
-java -jar target/${NAME}-${VERSION}.jar
+#java -jar target/${NAME}-${VERSION}.jar
